98a437f6133204c13f404a969f52d7923649a07a
tiago <tiagogarciaq@gmail.com>
2023-11-02 15:53:07 +0000

    coms

--- Client/src/pt/isec/pa/projetopd/cliente/model/data/ClientData.java
+++ Client/src/pt/isec/pa/projetopd/cliente/model/data/ClientData.java
@@ -4,16 +4,9 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class ClientData {
-    private static int CONNECTION_PORT; //Porto TCP
-    private static String IP_ADDRESS_OF_SERVER;
+    public static int CONNECTION_PORT; //Porto TCP
+    public static String IP_ADDRESS_OF_SERVER;
     private final Socket socket = null;
 
 
-    public static String getIpAddressOfServer() {
-        return IP_ADDRESS_OF_SERVER;
-    }
-
-    public static int getConnectionPort() {
-        return CONNECTION_PORT;
-    }
 }
