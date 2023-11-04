4e7dc26f0b5b6ab6a5e65c99e550a1aa15c19b51
tiago <tiagogarciaq@gmail.com>
2023-11-04 19:16:46 +0000

    tcp and manager

--- Client/src/pt/isec/pd/projetopd/cliente/model/data/communication/TCPReceive.java
+++ Client/src/pt/isec/pd/projetopd/cliente/model/data/communication/TCPReceive.java
@@ -10,21 +10,22 @@ public class TCPReceive extends Thread {
     private MessageReceivedListener listener;
     private final String ip;
     private final int port;
-    public TCPReceive(String ip, int port,MessageReceivedListener listener) {
+
+    public TCPReceive(String ip, int port, MessageReceivedListener listener) {
         this.ip = ip;
         this.port = port;
         this.listener = listener;
     }
+
     @Override
     public void run() {
         Socket socket = null;
 
         try {
             socket = new Socket(InetAddress.getByName(ip), port);
-
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
             String response;
+
             while ((response = reader.readLine()) != null) {
                 if (listener != null) {
                     listener.onMessageReceived(response);
@@ -42,7 +43,6 @@ public class TCPReceive extends Thread {
             }
         }
     }
-
 
     public interface MessageReceivedListener {
         void onMessageReceived(String message);
