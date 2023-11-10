4b2b8055890e475ba7f470ea5a7b3c5f249d855d
tiago <tiagogarciaq@gmail.com>
2023-11-10 15:37:28 +0000

    port class

--- CommunicationClasses/src/pt/isec/pd/projetopd/communication/classes/Port.java
+++ CommunicationClasses/src/pt/isec/pd/projetopd/communication/classes/Port.java
@@ -3,7 +3,7 @@ package pt.isec.pd.projetopd.communication.classes;
 import java.io.Serializable;
 
 public class Port implements Serializable {
-    String port;
-    public Port(String p){ this.port = p;}
-    public String getPort(){ return this.port;}
+    int port;
+    public Port(int p){ this.port = p;}
+    public int getPort(){ return this.port;}
 }
