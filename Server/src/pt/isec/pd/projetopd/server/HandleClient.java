397ddb82642501c1d2601dc91c92f0e163d333b5
Quevin Moderno <a2019135563@isec.pt>
2023-11-10 12:13:07 +0000

    server notifications sent

--- Server/src/pt/isec/pd/projetopd/server/HandleClient.java
+++ Server/src/pt/isec/pd/projetopd/server/HandleClient.java
@@ -1,21 +1,31 @@
 package pt.isec.pd.projetopd.server;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HandleClient implements Runnable {
 
     private Socket socket;
     private ServerInfo serverInfo;
+    private ServerSocket NotifSocket = null;
 
     public HandleClient(Socket sock, ServerInfo serverInfo) {
         this.socket = sock;
         this.serverInfo = serverInfo;
+
+        try{
+            this.NotifSocket = new ServerSocket(7001);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public void run() {
         System.out.println("I started");
+        sendNotifications("7001");
         while(true) {
             try (ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
                  ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
@@ -38,5 +48,17 @@ public class HandleClient implements Runnable {
                 }
             }
         }
+    }
+
+    private void sendNotifications(Object o) {
+        try (ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())
+        ) {
+            out.writeObject(o);
+            out.flush();
+            out.reset();
+        } catch (IOException e) {
+            System.out.println("<" + Thread.currentThread().getName() + ">:\n\t" + e);
+        }
+
     }
 }
